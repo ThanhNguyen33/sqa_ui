dec4fe5adff5d10075f15fa075d86f296c5b0867
ThanhNguyen33 <104722984+ThanhNguyen33@users.noreply.github.com>
2023-03-17 22:11:01 +0700

    base

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,5 +1,5 @@
-import { Box, Heading, Text, VStack } from "@hope-ui/solid";
-import { Component, createEffect, JSX, Show, splitProps } from "solid-js";
+import { Box, Text, VStack } from "@hope-ui/solid";
+import { Component, createSignal, JSX, Show, splitProps } from "solid-js";
 import style from "./SideBar.module.css";
 import _ from "lodash";
 
@@ -11,7 +11,7 @@ const SideBar : Component<SideBarProps> = (props) => {
 
   const [dataStyle] = splitProps(props, ["showSideBar"])
 
-  const dashBoardKey = [
+  const [dashBoardKey, setDashBoardKey] = createSignal([
     {
       label: "Dashboard",
       icon: "fa-solid fa-house",
@@ -19,6 +19,7 @@ const SideBar : Component<SideBarProps> = (props) => {
     {
       label: "My wallet",
       icon: "fa-solid fa-wallet",
+      show_childs: false,
       childs: [
         {
           label: "User wallet",
@@ -37,32 +38,58 @@ const SideBar : Component<SideBarProps> = (props) => {
     {
       label: "Settings",
       icon: "fa-solid fa-gear",
+      show_childs: false,
+      childs: [
+        {
+          label: "General Settings",
+          icon: "fa-solid fa-list-check",
+        },
+        {
+          label: "FAQs",
+          icon: "fa-solid fa-clipboard-question",
+        },
+        {
+          label: "Contact",
+          icon: "fa-solid fa-phone",
+        }
+      ]
     }
-  ]
+  ])
 
-    return (
-        <Box className={`${dataStyle.showSideBar ? style.slide_out_animation : style.hide_sidebar} vh-100`} backgroundColor="#212e55">
-          <Box height="20vh" minHeight="100px">
-            This is the side bar
-          </Box>
-          <VStack className="d-flex justify-content-center align-items-center">
-            {
-              dashBoardKey.map((item) => {
-                return (
-                  <div class={`${item.label} ${style.dashboard_menu_item} d-flex justify-content-flex-start align-items-center w-100 pl-5`}>
+  const setShowChildsComponent = (index: number) => {
+    const newDashBoardKey = _.chain(dashBoardKey()).map((item, idx) => {
+      if(idx === index){
+        item.show_childs = !item.show_childs
+      }
+      return item
+    }).toArray().value()
+    setDashBoardKey(newDashBoardKey)
+  }
+
+  return (
+      <Box className={`${dataStyle.showSideBar ? style.slide_out_animation : style.hide_sidebar} vh-100`} backgroundColor="#212e55">
+        <Box height="15vh" minHeight="70px">
+          
+        </Box>
+        <VStack className="d-flex justify-content-center align-items-center">
+          {
+            dashBoardKey().map((item, index) => {
+              return (
+                <div class={`${item.label} ${style.dashboard_menu_item} w-100 pl-4 pr-1`}>
+                  <div class="d-flex justify-content-flex-start align-items-center" onClick={() => setShowChildsComponent(index)} style={{
+                    "min-height": "50px",
+                    position: "relative",
+                  }}>
                     <i class={item.icon} style={{
-                      color: "#a3a8c8"
+                      color: "inherit",
                     }}/>
                     <Text 
                       cursor="default" 
                       margin={0} 
-                      color="#a3a8c8"
+                      color="inherit"
                       fontWeight={600}
                       size="xl"
-                      paddingLeft={15} 
-                      _hover={{
-                        color: "white"
-                      }}
+                      paddingLeft={15}
                     >
                       {item.label}
                     </Text>
@@ -70,18 +97,51 @@ const SideBar : Component<SideBarProps> = (props) => {
                       when={item.childs}
                       fallback={<></>}
                     >
-                      <i class="fa-solid fa-caret-down" style={{
-                        color: "#a3a8c8",
-                        "margin-left": "25px"
+                      <i class={`${style.menu_icon} fa-solid fa-caret-down`} style={{
+                        color: "inherit",
+                        position: "absolute",
+                        right: "20px",
+                        ...(item.show_childs && {
+                          transform: "rotate(90deg)"
+                        })
                       }}/>
                     </Show>
                   </div>
-                )
-              })
-            }
-          </VStack>
-        </Box>
-    )
+                  <div>
+                    <Show
+                      when={item.childs && item.show_childs}
+                      fallback={<></>}
+                    >
+                      {
+                        item.childs?.map(it => {
+                          return(
+                            <div class={`${it.label} ${style.dashboard_menu_item_child} d-flex justify-content-flex-start align-items-center w-100 pl-4`}>
+                              <i class={it.icon} style={{
+                                color: "inherit",
+                              }}/>
+                              <Text 
+                                cursor="default" 
+                                margin={0} 
+                                color="inherit"
+                                fontWeight={600}
+                                size="xl"
+                                paddingLeft={15}
+                              >
+                                {it.label}
+                              </Text>
+                            </div>
+                          )
+                        })
+                      }
+                    </Show>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </VStack>
+      </Box>
+  )
 }
 
 export default SideBar;
